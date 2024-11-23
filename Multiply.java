db4a94640b9f5da4db88b8fed0518bb2dd62506b
sathwika <ponagantisathwika>
2024-11-23 13:48:38 +0530

    Multiply java program

---
+++ Multiply.java
@@ -0,0 +1,9 @@
+public class Multiplication {
+    public static void main(String args[]) {
+        int num1 = 10;
+        int num2 = 20;
+        int product = num1 * num2;
+
+        System.out.println("Product of " + num1 + " and " + num2 + " is: " + product);
+    }
+}
